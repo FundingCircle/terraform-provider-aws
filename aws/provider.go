348b9d269cf067d2913dde7bb5507368d0f2e971
Gaylord Mazelier <gaylord@mazelier.fr>
2020-05-10 12:30:38 +0200

    New resource: aws_lakeformation_resource

--- aws/provider.go
+++ aws/provider.go
@@ -651,6 +651,7 @@ func Provider() terraform.ResourceProvider {
 			"aws_kms_key":                                             resourceAwsKmsKey(),
 			"aws_kms_ciphertext":                                      resourceAwsKmsCiphertext(),
 			"aws_lakeformation_datalake_settings":                     resourceAwsLakeFormationDataLakeSettings(),
+			"aws_lakeformation_resource":                              resourceAwsLakeFormationResource(),
 			"aws_lambda_alias":                                        resourceAwsLambdaAlias(),
 			"aws_lambda_event_source_mapping":                         resourceAwsLambdaEventSourceMapping(),
 			"aws_lambda_function_event_invoke_config":                 resourceAwsLambdaFunctionEventInvokeConfig(),
